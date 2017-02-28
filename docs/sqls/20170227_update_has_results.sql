e491830bb7aee78df3702f3e8eab1d4aaff6b470
Kitutu <pkitutu@gmail.com>
2017-02-28 20:37:57 +0300

    sm change2

--- docs/sqls/20170227_update_has_results.sql
+++ docs/sqls/20170227_update_has_results.sql
@@ -1,1 +1,1 @@
-UPDATE vl_samples_worksheetcredentials SET stage = 'has_results' WHERE id IN (SELECT worksheetID FROM vl_results_roche) ) OR id IN (SELECT worksheetID FROM vl_results_abbott);
+UPDATE vl_samples_worksheetcredentials SET stage = 'has_results' WHERE id IN (SELECT worksheetID FROM vl_results_roche) OR id IN (SELECT worksheetID FROM vl_results_abbott);
