2a86c87365b9aefa4ee94ae8c94d517782434e26
Simon Peter Muwanga <smuwanga@gmail.com>
2017-04-24 10:52:46 +0300

    Age bands overlaps removed

--- app/Console/Commands/MyName.php
+++ app/Console/Commands/MyName.php
@@ -31,13 +31,16 @@ class MyName extends Command{
     	//$this->comment("Your name is $x\n") ;
 
     	//$this->comment(Dashboard::find(90)->valid_results);
-        $age=-1;
+        $age=30;
         
         $arr=[];
         for ($index=1; $index < 100; $index ++) { 
-            $from_age = $index - 1;
+            $from_age = $index;
             $to_age = $index;
-            $arr[$index]="$age >=$from_age && $age < $to_age";
+            if($age >=$from_age && $age < $to_age){
+                $arr[$index]="$age <= $from_age && $age >= $to_age";
+            }
+            
 
             # code...
         }
