e1aa7d5556d52510bb23085abfffa1ac606328ac
Kitutu <pkitutu@gmail.com>
2017-05-15 12:20:52 +0300

    round off

--- app/Closet/MyHTML.php
+++ app/Closet/MyHTML.php
@@ -431,7 +431,7 @@ class MyHTML{
 			$suppressed = 'YES';
 			$alpha_numerical_result = substr($result, 0,1)." 20 Copies / mL";
 		}else{
-			$numerical_result = number_format((float)$result, 2);
+			$numerical_result = number_format((float)$result);
 			$suppressed = $numerical_result>1000?'NO':'YES';
 			$alpha_numerical_result = "$numerical_result Copies / mL";
 		}
