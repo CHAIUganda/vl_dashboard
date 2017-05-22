6b4e086e1632b354cb50791a6bb4f5c58d6b38b6
Kitutu <pkitutu@gmail.com>
2017-05-22 15:21:47 +0300

    d

--- docs/sqls/20170427_dhis2.sql
+++ docs/sqls/20170427_dhis2.sql
@@ -1,12 +1,9 @@
---create two columns for vl_districts
 Alter table vl_districts add column dhis2_uid varchar(12) after district; 
 Alter table vl_districts add column dhis2_name varchar(40) after district;
 
---create two columns for vl_facilities
 Alter table vl_facilities add column district_uid varchar(12) after facility; 
 Alter table vl_facilities add column dhis2_uid varchar(12) after facility; 
 Alter table vl_facilities add column dhis2_name varchar(40) after facility;
 
----hard coded vl_districts
 UPDATE vl_districts SET dhis2_name='Sembabule District', dhis2_uid='j7AQsnEYmvi' WHERE id=82;
 UPDATE vl_districts SET dhis2_name='Maracha District', dhis2_uid='WyR8Eetj7Uw' WHERE id=58;
