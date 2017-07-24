a589c90a2c432f8449e2c5c4bb5bd484924ab16a
Simon Peter Muwanga <smuwanga@gmail.com>
2017-07-24 15:23:00 +0300

    Roles for facility access

--- docs/sqls/20170713_reports_permissions.sql
+++ docs/sqls/20170713_reports_permissions.sql
@@ -15,3 +15,21 @@ name,display_name ,description ,created_at,updated_at)
 VALUES ( 'view_reports_as_admin', 'View Reports as Admin', 'Can view all reports for hubs and facilities', 
 '2017-07-14 15:00:00', '0000-00-00 00:00:00'
 );
+
+INSERT INTO roles (
+name,display_name ,description ,created_at,updated_at)
+VALUES ( 'view_facility_reports', 'View Facility Reports', 'Can view reports for a particular facility', 
+'2017-07-13 00:00:00', '0000-00-00 00:00:00'
+);
+
+INSERT INTO roles (
+name,display_name ,description ,created_at,updated_at)
+VALUES ( 'view_reports_as_hub', 'View Reports as Hub', 'Can view reports as a Hub', 
+'2017-07-13 17:00:00', '0000-00-00 00:00:00'
+);
+
+INSERT INTO roles (
+name,display_name ,description ,created_at,updated_at)
+VALUES ( 'view_reports_as_admin', 'View Reports as Admin', 'Can view reports as an Admin', 
+'2017-07-13 17:00:00', '0000-00-00 00:00:00'
+);
