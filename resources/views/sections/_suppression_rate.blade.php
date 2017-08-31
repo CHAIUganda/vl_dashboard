43b08ceed041aa018498980f37645738970ec498
Simon Peter Muwanga <smuwanga@gmail.com>
2017-08-31 17:32:46 +0300

    added Non Suppresing Results to the csv downloads

--- resources/views/sections/_suppression_rate.blade.php
+++ resources/views/sections/_suppression_rate.blade.php
@@ -64,11 +64,11 @@
          </div>
          <br>
          <br>
-         <button ng-hide="show_fclties2" id="exportDistrictsSuppressionRate" type="button" ng-csv="export_district_suppression_numbers"  class="btn btn-success" filename="district_suppression_<%current_timestamp%>.csv" csv-header="['District','Suppressed Results', 'Valid Results', 'Suppression Rate (%)']">Download CSV</button>
+         <button ng-hide="show_fclties2" id="exportDistrictsSuppressionRate" type="button" ng-csv="export_district_suppression_numbers"  class="btn btn-success" filename="district_suppression_<%current_timestamp%>.csv" csv-header="['District' ,'Valid Results','Suppressed Results', 'Non Suppressed Results','Suppression Rate (%)']">Download CSV</button>
 
          <br>
          <br>
-         <button ng-show="show_fclties2" id="exportFacilitiesSuppressionRate" type="button" ng-csv="export_facility_suppression_numbers" filename="facilities_suppression_<%current_timestamp%>.csv" class="btn btn-success" csv-header="['District','Hub','Facility','Suppressed Results', 'Valid Results', 'Suppression Rate (%)']">Download CSV</button>
+         <button ng-show="show_fclties2" id="exportFacilitiesSuppressionRate" type="button" ng-csv="export_facility_suppression_numbers" filename="facilities_suppression_<%current_timestamp%>.csv" class="btn btn-success" csv-header="['District','Hub','Facility','Valid Results','Suppressed Results','Non Suppressed Results','Suppression Rate (%)']">Download CSV</button>
 
     </div>
 </div> 
