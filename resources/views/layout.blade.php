89cfa31a50c216ce4f87ff15d37549ea77d70a91
Simon Peter Muwanga <smuwanga@gmail.com>
2019-06-07 09:39:10 +0300

    Rejections restored

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -43,8 +43,7 @@
 
     <script src="{{ asset('/js/general.js') }}" type="text/javascript"></script>
    
-    <script src="{{ asset('/js/d3.min.js') }}" charset="utf-8"></script>
-    <script src="{{ asset('/js/nv.d3.min.js') }}"></script>
+    
     <script type="text/javascript">
         function activeRetestNSTab () {
             //remove class
@@ -101,6 +100,9 @@
 <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.3.js"></script>
 
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.2/d3.min.js"></script>
+<script type="text/javascript" src="https://nvd3-community.github.io/nvd3/build/nv.d3.js"></script>
 
 <link href="{{ asset('/css/hub.css') }}" rel="stylesheet"/>
 <style type="text/css">
